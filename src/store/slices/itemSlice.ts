d309a38ca4a7c13b29292516caf954a06ef0f754
LBJISGOAT123 <lashangri4434@gmail.com>
2026-01-01 20:40:49 +0900

    Update itemSlice.ts

--- src/store/slices/itemSlice.ts
+++ src/store/slices/itemSlice.ts
@@ -1,21 +1,19 @@
-import { StateCreator } from 'zustand';
-import { GameStore, ItemSlice } from '../types';
-import { INITIAL_ITEMS } from '../../data/items';
-import { Item } from '../../types';
+// ==========================================
+// FILE PATH: /src/store/slices/itemSlice.ts
+// ==========================================
 
-export interface ItemSlice {
-  shopItems: Item[];
-  addItem: (item: Item) => void;
-  deleteItem: (id: string) => void;
-  updateItem: (id: string, updates: Partial<Item>) => void;
-}
+import { StateCreator } from 'zustand';
+import { GameStore, ItemSlice, Item } from '../types';
+import { INITIAL_ITEMS } from '../../data/items';
 
 export const createItemSlice: StateCreator<GameStore, [], [], ItemSlice> = (set) => ({
   shopItems: INITIAL_ITEMS,
 
-  addItem: (item) => set((state) => ({ shopItems: [...state.shopItems, item] })),
-  deleteItem: (id) => set((state) => ({ shopItems: state.shopItems.filter(i => i.id !== id) })),
-  updateItem: (id, updates) => set((state) => ({
+  addItem: (item: Item) => set((state) => ({ shopItems: [...state.shopItems, item] })),
+  
+  deleteItem: (id: string) => set((state) => ({ shopItems: state.shopItems.filter(i => i.id !== id) })),
+  
+  updateItem: (id: string, updates: Partial<Item>) => set((state) => ({
     shopItems: state.shopItems.map(i => i.id === id ? { ...i, ...updates } : i)
   })),
 });
