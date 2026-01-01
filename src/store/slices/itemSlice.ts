7bbd89942250ca44488f486a9417853510978d9a
LBJISGOAT123 <lashangri4434@gmail.com>
2026-01-01 20:53:37 +0900

    Update itemSlice.ts

--- src/store/slices/itemSlice.ts
+++ src/store/slices/itemSlice.ts
@@ -1,19 +1,16 @@
-// ==========================================
-// FILE PATH: /src/store/slices/itemSlice.ts
-// ==========================================
-
 import { StateCreator } from 'zustand';
-import { GameStore, ItemSlice, Item } from '../types';
+import { GameStore, ItemSlice } from '../types';
 import { INITIAL_ITEMS } from '../../data/items';
+import { Item } from '../../types';
 
 export const createItemSlice: StateCreator<GameStore, [], [], ItemSlice> = (set) => ({
   shopItems: INITIAL_ITEMS,
 
   addItem: (item: Item) => set((state) => ({ shopItems: [...state.shopItems, item] })),
   
-  deleteItem: (id: string) => set((state) => ({ shopItems: state.shopItems.filter(i => i.id !== id) })),
+  deleteItem: (id: string) => set((state) => ({ shopItems: state.shopItems.filter((i) => i.id !== id) })),
   
   updateItem: (id: string, updates: Partial<Item>) => set((state) => ({
-    shopItems: state.shopItems.map(i => i.id === id ? { ...i, ...updates } : i)
+    shopItems: state.shopItems.map((i) => i.id === id ? { ...i, ...updates } : i)
   })),
 });
