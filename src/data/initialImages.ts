d8a06efc32327e921448eeeb5d61c8b342239a2e
LBJISGOAT123 <lashangri4434@gmail.com>
2026-01-02 10:32:07 +0900

    Update initialImages.ts

--- src/data/initialImages.ts
+++ src/data/initialImages.ts
@@ -3,65 +3,79 @@
 // ==========================================
 
 export const INITIAL_CUSTOM_IMAGES: Record<string, string> = {
-  // === 1. 신(진영) ===
+  // ==============================================
+  // 1. 신 (진영) 및 역할군 아이콘
+  // ==============================================
   'god_izman': '/images/gods/god_izman.png',
   'god_dante': '/images/gods/god_dante.png',
 
-  // === 2. 직업 아이콘 & 배경 ===
-  // 집행관
   'role_executor': '/images/roles/role_executor.png',
-  'role_executor_bg': '/images/roles/role_executor_bg.png',
-  // 추적자
   'role_tracker': '/images/roles/role_tracker.png',
-  'role_tracker_bg': '/images/roles/role_tracker_bg.png',
-  // 선지자
   'role_prophet': '/images/roles/role_prophet.png',
-  'role_prophet_bg': '/images/roles/role_prophet_bg.png',
-  // 신살자
   'role_slayer': '/images/roles/role_slayer.png',
-  'role_slayer_bg': '/images/roles/role_slayer_bg.png',
-  // 수호기사
   'role_guardian': '/images/roles/role_guardian.png',
-  'role_guardian_bg': '/images/roles/role_guardian_bg.png',
 
-  // === 3. 영웅 프로필 (예시) ===
-  // 집행관
+  // ==============================================
+  // 2. 영웅 (Heroes) - 총 50명
+  // ==============================================
+
+  // [집행관: Executor]
   'h_ragna': '/images/heroes/h_ragna.png',
   'h_kensei': '/images/heroes/h_kensei.png',
   'h_baldur': '/images/heroes/h_baldur.png',
   'h_freya': '/images/heroes/h_freya.png',
   'h_gorgon': '/images/heroes/h_gorgon.png',
   'h_arthur': '/images/heroes/h_arthur.png',
+  'h_leonidas': '/images/heroes/h_leonidas.png',
+  'h_musashi': '/images/heroes/h_musashi.png',
+  'h_lancelot': '/images/heroes/h_lancelot.png',
+  'h_siegfried': '/images/heroes/h_siegfried.png',
 
-  // 선지자
+  // [선지자: Prophet]
   'h_merlin': '/images/heroes/h_merlin.png',
   'h_crowley': '/images/heroes/h_crowley.png',
   'h_elara': '/images/heroes/h_elara.png',
   'h_nix': '/images/heroes/h_nix.png',
   'h_sol': '/images/heroes/h_sol.png',
   'h_gaia': '/images/heroes/h_gaia.png',
+  'h_nostra': '/images/heroes/h_nostra.png',
+  'h_rasputin': '/images/heroes/h_rasputin.png',
+  'h_circe': '/images/heroes/h_circe.png',
+  'h_morgana': '/images/heroes/h_morgana.png',
 
-  // 추적자
+  // [추적자: Tracker]
   'h_kage': '/images/heroes/h_kage.png',
   'h_fenrir': '/images/heroes/h_fenrir.png',
   'h_viper': '/images/heroes/h_viper.png',
   'h_specter': '/images/heroes/h_specter.png',
   'h_locust': '/images/heroes/h_locust.png',
   'h_scarlet': '/images/heroes/h_scarlet.png',
+  'h_hattori': '/images/heroes/h_hattori.png',
+  'h_jack': '/images/heroes/h_jack.png',
+  'h_arachne': '/images/heroes/h_arachne.png',
+  'h_goemon': '/images/heroes/h_goemon.png',
 
-  // 수호기사
+  // [수호기사: Guardian]
   'h_aigis': '/images/heroes/h_aigis.png',
   'h_golem': '/images/heroes/h_golem.png',
   'h_paladin': '/images/heroes/h_paladin.png',
   'h_treant': '/images/heroes/h_treant.png',
   'h_magnus': '/images/heroes/h_magnus.png',
   'h_yeti': '/images/heroes/h_yeti.png',
+  'h_spartacus': '/images/heroes/h_spartacus.png',
+  'h_titan': '/images/heroes/h_titan.png',
+  'h_behemoth': '/images/heroes/h_behemoth.png',
+  'h_tortuga': '/images/heroes/h_tortuga.png',
 
-  // 신살자
+  // [신살자: God Slayer]
   'h_hawk': '/images/heroes/h_hawk.png',
   'h_trigger': '/images/heroes/h_trigger.png',
   'h_nova': '/images/heroes/h_nova.png',
   'h_flint': '/images/heroes/h_flint.png',
   'h_sylvia': '/images/heroes/h_sylvia.png',
   'h_gambit': '/images/heroes/h_gambit.png',
+  'h_robin': '/images/heroes/h_robin.png',
+  'h_artemis': '/images/heroes/h_artemis.png',
+  'h_apollo': '/images/heroes/h_apollo.png',
+  'h_kaiser': '/images/heroes/h_kaiser.png',
 };
